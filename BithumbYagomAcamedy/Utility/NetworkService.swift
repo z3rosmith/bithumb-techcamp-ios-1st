726e61e5d8d29436346520d116fb14e48988c9d8
HJEHA <hyhpwang@gmail.com>
2022-02-24 13:27:57 +0900

    feat: NetworkService loadData() 구현 #3
    - NetworkError 열거형 구현

--- BithumbYagomAcamedy/Utility/NetworkService.swift
+++ BithumbYagomAcamedy/Utility/NetworkService.swift
@@ -7,10 +7,51 @@
 
 import Foundation
 
+enum NetworkError: LocalizedError {
+    case statusCodeError
+    case unknown(error: Error)
+
+    var errorDescription: String? {
+        switch self {
+        case .statusCodeError:
+            return "정상적인 StatusCode가 아닙니다."
+        case .unknown(let error):
+            return "\(error.localizedDescription) 에러가 발생했습니다."
+        }
+    }
+}
+
 struct NetworkService {
     private let session: URLSession
     
     init(session: URLSession = URLSession.shared) {
         self.session = session
+    }
+    
+    private func loadData(
+        urlRequest: URLRequest,
+        completionHandler: @escaping ((Result<Data, NetworkError>) -> Void)
+    ) {
+        let task = session.dataTask(with: urlRequest) { data, response, error in
+            if let error = error {
+                completionHandler(.failure(.unknown(error: error)))
+                return
+            }
+            
+            let successStatusCode = 200..<300
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  successStatusCode.contains(httpResponse.statusCode)
+            else {
+                completionHandler(.failure(.statusCodeError))
+                return
+            }
+            
+            if let data = data {
+                completionHandler(.success(data))
+                return
+            }
+        }
+        task.resume()
     }
 }
