bf49f2f98a09d560840fb1cbaaa9665bb07bc3b5
Oh Donggeon <donggeon.dev@gmail.com>
2022-02-24 17:48:39 +0900

    feat: 코인 리스트 컬렉션 뷰 셀 클래스 추가 및 xib 레이아웃 이동
    - xib 파일을 하나 만들어서 작업하였으나 나중에 문제가 생길 것 같아 Cocoa class 만들면서 xib 만들어 주었습니다.
    - 이전 작성했던 레이아웃을 새로운 xib로 옮겨주었습니다.

---
+++ BithumbYagomAcamedy/View/CoinListView/CoinListCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CoinListCollectionViewCell.swift
+//  BithumbYagomAcamedy
+//
+//  Created by Oh Donggeon on 2022/02/24.
+//
+
+import UIKit
+
+class CoinListCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
