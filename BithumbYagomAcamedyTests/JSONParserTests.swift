670dbb30ec208eae2671c50fbebd7f400921df52
Oh Donggeon <donggeon.dev@gmail.com>
2022-02-25 14:18:33 +0900

    test: JSONParser를 테스트하기 위한 테스트 코드 작성 #16
    Ticker, Tickers, Orderbook, Transaction, AssetStatus 타입으로 디코딩하는 테스트 코드 작성

--- BithumbYagomAcamedyTests/JSONParserTests.swift
+++ BithumbYagomAcamedyTests/JSONParserTests.swift
@@ -6,9 +6,56 @@
 //
 
 import XCTest
+@testable import BithumbYagomAcamedy
 
 class JSONParserTests: XCTestCase {
-    func test_bithumb_public_api_ticker_json_data_decode_then_() throws {
+    private var parser: JSONParser?
+    
+    override func setUpWithError() throws {
+        parser = JSONParser()
+    }
+    
+    override func tearDownWithError() throws {
+        parser = nil
+    }
+    
+    func test_ticker_json_파일을_디코딩_시_ticker_value_object_타입의_인스턴스_반환() throws {
+        let url = try XCTUnwrap(Bundle(for: type(of: self))
+                                    .url(forResource: "TickerJSONFile", withExtension: "json"))
+        let data = try XCTUnwrap(Data(contentsOf: url))
         
+        XCTAssertNoThrow(try parser?.decode(data: data, type: TickerValueObject.self))
+    }
+    
+    func test_tickers_json_파일을_디코딩_시_tickers_value_object_타입의_인스턴스_반환() throws {
+        let url = try XCTUnwrap(Bundle(for: type(of: self))
+                                    .url(forResource: "TickersJSONFile", withExtension: "json"))
+        let data = try XCTUnwrap(Data(contentsOf: url))
+        
+        XCTAssertNoThrow(try parser?.decode(data: data, type: TickersValueObject.self))
+    }
+    
+    func test_orderbook_json_파일을_디코딩_시_orderbook_value_object_타입의_인스턴스_반환() throws {
+        let url = try XCTUnwrap(Bundle(for: type(of: self))
+                                    .url(forResource: "OrderbookJSONFile", withExtension: "json"))
+        let data = try XCTUnwrap(Data(contentsOf: url))
+        
+        XCTAssertNoThrow(try parser?.decode(data: data, type: OrderbookValueObject.self))
+    }
+    
+    func test_transaction_json_파일을_디코딩_시_transaction_value_object_타입의_인스턴스_반환() throws {
+        let url = try XCTUnwrap(Bundle(for: type(of: self))
+                                    .url(forResource: "TransactionJSONFile", withExtension: "json"))
+        let data = try XCTUnwrap(Data(contentsOf: url))
+        
+        XCTAssertNoThrow(try parser?.decode(data: data, type: TranscationValueObject.self))
+    }
+    
+    func test_assets_status_json_파일을_디코딩_시_assets_status_all_value_object_타입의_인스턴스_반환() throws {
+        let url = try XCTUnwrap(Bundle(for: type(of: self))
+                                    .url(forResource: "AssetsStatusJSONFile", withExtension: "json"))
+        let data = try XCTUnwrap(Data(contentsOf: url))
+        
+        XCTAssertNoThrow(try parser?.decode(data: data, type: AssetsStatusValueObject.self))
     }
 }
