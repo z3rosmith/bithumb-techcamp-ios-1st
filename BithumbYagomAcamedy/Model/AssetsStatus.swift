47b168ec53ca2ff37bbf525310379dcc5dc69bfa
Oh Donggeon <donggeon.dev@gmail.com>
2022-02-28 14:32:01 +0900

    feat: 네트워크 통신을 통한 입출금 현황 데이터를 화면에 표시 #27 입출금 현황 화면에 대한 MVC 아키텍쳐 설계 Bithumb public Assets status API를 통해 입출금 현황 데이터를 가져온 후 컬렉션 뷰 화면에 표시

---
+++ BithumbYagomAcamedy/Model/AssetsStatus.swift
@@ -0,0 +1,26 @@
+//
+//  AssetsStatus.swift
+//  BithumbYagomAcamedy
+//
+//  Created by Oh Donggeon on 2022/02/28.
+//
+
+import Foundation
+
+struct AssetsStatus: DepositWithdrawalCellDataProviding, Hashable {
+    private(set) var coinName: String
+    private(set) var coinSymbol: String
+    private(set) var depositStatus: String
+    private(set) var withdrawalStatus: String
+    private(set) var isValidDeposit: Bool
+    private(set) var isValidWithdrawal: Bool
+    let uuid: UUID = UUID()
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(uuid)
+    }
+    
+    static func ==(lhs: AssetsStatus, rhs: AssetsStatus) -> Bool {
+        return lhs.uuid == rhs.uuid
+    }
+}
