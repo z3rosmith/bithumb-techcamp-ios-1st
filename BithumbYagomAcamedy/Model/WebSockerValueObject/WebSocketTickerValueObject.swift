20ee54b5a6ff57e0950c2fa0cbd877b00499cd02
HwangJeHa <hyhpwang@gmail.com>
2022-03-01 14:21:50 +0900

    feat: WebSocketTickerValueObjcet 구현 #35

---
+++ BithumbYagomAcamedy/Model/WebSockerValueObject/WebSocketTickerValueObject.swift
@@ -0,0 +1,58 @@
+//
+//  WebSocketTickerValueObject.swift
+//  BithumbYagomAcamedy
+//
+//  Created by 황제하 on 2022/03/01.
+//
+
+import Foundation
+
+struct WebSocketTickerValueObjcet: Decodable {
+    let type: String
+    let webSocketTickerData: WebSocketTickerData
+    
+    enum CodingKeys: String, CodingKey {
+        case type
+        case webSocketTickerData = "content"
+    }
+}
+
+struct WebSocketTickerData: Decodable {
+    let symbol: String
+    let tickType: String
+    let date: String
+    let time: String
+    let openPrice: String
+    let closePrice: String
+    let lowPrice: String
+    let highPrice: String
+    let value: String
+    let volume: String
+    let sellVolume: String
+    let buyVolume: String
+    let prevClosePrice: String
+    let changeRate: String
+    let changePrice: String
+    let volumePower: String
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case symbol
+        case tickType
+        case date
+        case time
+        case openPrice
+        case closePrice
+        case lowPrice
+        case highPrice
+        case value
+        case volume
+        case sellVolume
+        case buyVolume
+        case volumePower
+
+        case prevClosePrice
+        case changeRate = "chgRate"
+        case changePrice = "chgAmt"
+    }
+}
