a12269fff1261b704a1f33df35aea980f5f0c690
Oh Donggeon <donggeon.dev@gmail.com>
2022-03-02 23:20:18 +0900

    feat: Chart SPM 추가 및 코인 차트 뷰 추가 및 클래스 초기화 #50

---
+++ BithumbYagomAcamedy/Controller/CoinChartViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CoinChartViewController.swift
+//  BithumbYagomAcamedy
+//
+//  Created by Oh Donggeon on 2022/03/02.
+//
+
+import UIKit
+import Charts
+
+class CoinChartViewController: UIViewController {
+    @IBOutlet private weak var coinChartViewController: CandleStickChartView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
