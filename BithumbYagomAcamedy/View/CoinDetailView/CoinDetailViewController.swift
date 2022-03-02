9684903ebfb54e362762ed05be57d25c18949167
HwangJeHa <hyhpwang@gmail.com>
2022-03-02 15:03:53 +0900

    feat: 코인 디테일 뷰 초기 설정 #45
    - 코인 디테일 뷰에 코인(Coin) 데이터 전달
    - 코인 리스트 뷰 네비게이션 컨트롤러 추가
      - 네비게이션 바 활성화/비활성화 기능 구현
    - 페이지 뷰 컨트롤러 추가
    - 차트 뷰 스토리보드 추가
    - 호가 뷰 스토리보드 추가
    - 채결 내역 스토리보드 추가

---
+++ BithumbYagomAcamedy/View/CoinDetailView/CoinDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CoinDetailViewController.swift
+//  BithumbYagomAcamedy
+//
+//  Created by 황제하 on 2022/03/02.
+//
+
+import UIKit
+
+class CoinDetailViewController: UIViewController {
+
+    var coin: Coin?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
