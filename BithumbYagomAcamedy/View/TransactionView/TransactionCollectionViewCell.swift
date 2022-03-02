6cb9228ba4079b920e4f1dc799075ddb0e7d725f
HwangJeHa <hyhpwang@gmail.com>
2022-03-02 11:13:56 +0900

    feat: 채결내역 Cell 구현 #39
    - TransactionCollectionViewCell.xib 구성
    - TransactionCollectionViewCell Class 구현
    - Transaction 타입 구현
    - TransactionVO, WebSocketTransactionVO extension: Transaction 타입 생성 메서드

---
+++ BithumbYagomAcamedy/View/TransactionView/TransactionCollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TransactionCollectionViewCell.swift
+//  BithumbYagomAcamedy
+//
+//  Created by 황제하 on 2022/03/01.
+//
+
+import UIKit
+
+final class TransactionCollectionViewCell: UICollectionViewListCell {
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet private weak var dateLabel: UILabel!
+    @IBOutlet private weak var priceLabel: UILabel!
+    @IBOutlet private weak var quantityLabel: UILabel!
+    
+    // MARK: - Method
+    
+    func update(_ item: Transaction) {
+        dateLabel.text = item.convertedDate
+        priceLabel.text = item.price
+        quantityLabel.text = item.quantity
+        priceLabel.textColor = labelColor(type: item.type)
+        quantityLabel.textColor = labelColor(type: item.type)
+    }
+    
+    private func labelColor(type: TransactionType) -> UIColor {
+        return type == .bid ? UIColor.systemRed : UIColor.systemBlue
+    }
+}
