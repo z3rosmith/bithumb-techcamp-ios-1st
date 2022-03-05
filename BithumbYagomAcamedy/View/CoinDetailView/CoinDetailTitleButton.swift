2ff60acb9696eeb76b336ac3c20e1d861fe2e3f1
HwangJeHa <hyhpwang@gmail.com>
2022-03-05 17:48:32 +0900

    refactor: 차트 호가 채결내역 버튼 UnderLine Color 인스펙터 노출 #52

--- BithumbYagomAcamedy/View/CoinDetailView/CoinDetailTitleButton.swift
+++ BithumbYagomAcamedy/View/CoinDetailView/CoinDetailTitleButton.swift
@@ -17,7 +17,8 @@ final class CoinDetailTitleButton: UIButton {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        configureTitleLabel()
     }
     
     // MARK: - Method
