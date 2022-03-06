b771e5c11ffac466637fb6f9a41ee9bf74c63f99
HwangJeHa <hyhpwang@gmail.com>
2022-03-06 15:03:22 +0900

    chore: 호가 웹소켓 시작 함수 주석 처리 #55

--- BithumbYagomAcamedy/View/CoinDetailView/CoinOrderbookView/CoinOrderbookViewController.swift
+++ BithumbYagomAcamedy/View/CoinDetailView/CoinOrderbookView/CoinOrderbookViewController.swift
@@ -104,7 +104,7 @@ extension CoinOrderbookViewController {
     private func configureDataManager() {
         coinOrderbookDataManager.delegate = self
         coinOrderbookDataManager.fetchOrderbook()
-        coinOrderbookDataManager.fetchOrderbookWebSocket()
+//        coinOrderbookDataManager.fetchOrderbookWebSocket() // TODO: 구현 중
     }
 }
 
