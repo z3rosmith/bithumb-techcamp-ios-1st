a325decf4640950bf359533e4fe5a32cf7a533eb
HwangJeHa <hyhpwang@gmail.com>
2022-03-07 16:31:26 +0900

    feat: CoinDetailDataManager 차트 데이터 HTTP 통신 구현 #62

--- BithumbYagomAcamedy/View/CoinDetailView/CoinDetailChartImageView.swift
+++ BithumbYagomAcamedy/View/CoinDetailView/CoinDetailChartImageView.swift
@@ -8,22 +8,56 @@
 import UIKit
 
 final class CoinDetailChartImageView: UIImageView {
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        drawChart(price: [10, 20, 30, 20, 10, 0, 10])
+    
+    // MARK: - Method
+    
+    func drawChart(price: [Double]) {
+        guard let context = pathContext() else {
+            return
+        }
+        
+        let axisUnit = setAxisUnit(price: price)
+        let startPoint = CGPoint(
+            x: CGFloat.zero,
+            y: frame.height - (price[0] * axisUnit.y)
+        )
+        
+        var previousPoint = startPoint
+        context.move(to: previousPoint)
+        
+        for (index, price) in price.enumerated() {
+            let nextPoint = CGPoint(
+                x: axisUnit.x * CGFloat(index),
+                y: layer.frame.height - (axisUnit.y * CGFloat(price))
+            )
+            
+            setPathColor(from: previousPoint, to: nextPoint, with: context)
+            drawPath(from: previousPoint, to: nextPoint, with: context)
+            
+            previousPoint = nextPoint
+        }
+        
+        setImage(with: context)
     }
     
-    private func getContext() -> CGContext? {
+    private func pathContext() -> CGContext? {
         UIGraphicsBeginImageContext(layer.frame.size)
         let context = UIGraphicsGetCurrentContext()
         context?.beginPath()
         return context
     }
     
-    private func setImage(with context: CGContext) {
-        context.closePath()
-        self.image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
+    private func setAxisUnit(price: [Double]) -> (x: CGFloat, y: CGFloat) {
+        guard let min = price.min(),
+              let max = price.max()
+        else {
+            return (CGFloat.zero, CGFloat.zero)
+        }
+        
+        let xUnit = layer.frame.width / CGFloat(price.count)
+        let yUnit = layer.frame.height / (max - min)
+        
+        return (xUnit, yUnit)
     }
     
     private func drawPath(
@@ -48,44 +82,9 @@ final class CoinDetailChartImageView: UIImageView {
         }
     }
     
-    private func setAxisUnit(price: [Double]) -> (x: CGFloat, y: CGFloat) {
-        guard let min = price.min(),
-              let max = price.max()
-        else {
-            return (CGFloat.zero, CGFloat.zero)
-        }
-        
-        let xUnit = layer.frame.width / CGFloat(price.count)
-        let yUnit = layer.frame.height / (max - min)
-        
-        return (xUnit, yUnit)
-    }
-    
-    func drawChart(price: [Double]) {
-        guard let context = getContext() else {
-            return
-        }
-        
-        let axisUnit = setAxisUnit(price: price)
-        let startPoint = CGPoint(
-            x: CGFloat.zero,
-            y: frame.height - (price[0] * axisUnit.y)
-        )
-        var previousPoint = startPoint
-        context.move(to: previousPoint)
-        
-        for (index, price) in price.enumerated() {
-            let nextPoint = CGPoint(
-                x: axisUnit.x * CGFloat(index),
-                y: frame.height - (axisUnit.y * price)
-            )
-            
-            setPathColor(from: previousPoint, to: nextPoint, with: context)
-            drawPath(from: previousPoint, to: nextPoint, with: context)
-            
-            previousPoint = nextPoint
-        }
-        
-        setImage(with: context)
+    private func setImage(with context: CGContext) {
+        context.closePath()
+        self.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
     }
 }
