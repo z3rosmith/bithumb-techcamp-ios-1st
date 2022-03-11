fa6a2c22a4488483e0908c5929956cacac40f664
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2022-03-11 14:18:23 +0900

    fix: 코인 목록 화면 종가 기준 변경 #98 - 종가 기준 변경 - UICollectionViewDiffableDataSource extension 추가

---
+++ BithumbYagomAcamedy/Extension/UICollectionViewDiffableDataSource+extension.swift
@@ -0,0 +1,28 @@
+//
+//  CollectionViewDiffableDataSource+extension.swift
+//  BithumbYagomAcamedy
+//
+//  Created by Jinyoung Kim on 2022/03/11.
+//
+
+import UIKit
+
+extension UICollectionViewDiffableDataSource {
+    func applySnapshot(
+        _ snapshot: NSDiffableDataSourceSnapshot<SectionIdentifierType, ItemIdentifierType>,
+        animated: Bool,
+        completion: (() -> Void)? = nil
+    ) {
+        if #available(iOS 15.0, *) {
+            self.apply(snapshot, animatingDifferences: animated, completion: completion)
+        } else {
+            if animated {
+                self.apply(snapshot, animatingDifferences: true, completion: completion)
+            } else {
+                UIView.performWithoutAnimation {
+                    self.apply(snapshot, animatingDifferences: true, completion: completion)
+                }
+            }
+        }
+    }
+}
